a595d67bdc8f64a1e8042a426433cbaea52b4615
temp-msp <test_msp@outlook.com>
2017-04-04 12:48:49 +0700

    commit message

--- application/config/config.php
+++ application/config/config.php
@@ -41,7 +41,7 @@ _NAME']);
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
