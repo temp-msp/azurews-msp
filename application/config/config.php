d615cde68ad37d948be565b6ac8b033fa90e9f1b
temp-msp <test_msp@outlook.com>
2017-04-04 12:52:00 +0700

    commit message

--- application/config/config.php
+++ application/config/config.php
@@ -27,9 +27,7 @@ $config['base_url'] = 'http';
 if (@$_SERVER["HTTPS"] == "on") {$config['base_url'] .= "s";}
 $config['base_url'] .= "://";
 $config['base_url'] .= $_SERVER['HTTP_HOST'];
-$config['base_url'] .=
-str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT
-_NAME']);
+$config['base_url'] .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
 
 /*
 |--------------------------------------------------------------------------
